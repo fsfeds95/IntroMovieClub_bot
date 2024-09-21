1ffd8319249740db1047cca69fb31fbdae190a62
fsfeds95 <144530359+fsfeds95@users.noreply.github.com>
2024-09-21 19:57:00 -0400

    Actualizar

---
+++ index.js
@@ -0,0 +1,11 @@
+const BOT_TOKEN = '7799058013:AAE-HjGVOt5AbOKfdR-KGL66reWhdADPgn8';
+
+const { Telegraf } = require('telegraf');
+
+const bot = new Telegraf(BOT_TOKEN);
+
+bot.on('text', (ctx) => {
+    ctx.reply(ctx.message.text);
+});
+
+bot.launch();
