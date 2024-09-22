41fa8975715f1407ca6496dc7de5e48d9b50a417
fsfeds95 <144530359+fsfeds95@users.noreply.github.com>
2024-09-21 22:01:39 -0400

    Actualizar index.js

--- index.js
+++ index.js
@@ -1,10 +1,8 @@
-const { Telegraf } = require('telegraf');
-
 const BOT_TOKEN = '7799058013:AAE-HjGVOt5AbOKfdR-KGL66reWhdADPgn8';
 
 const { Telegraf } = require('telegraf');
 
-const bot = new Telegraf('BOT_TOKEN');
+const bot = new Telegraf(BOT_TOKEN);
 
 bot.on('text', (ctx) => {
     ctx.reply(ctx.message.text);
