6549859f0bb54037249d24783973d279ef674fa5
fsfeds95 <144530359+fsfeds95@users.noreply.github.com>
2024-09-21 21:15:58 -0400

    Actualizar index.js

--- index.js
+++ index.js
@@ -1,38 +1,14 @@
 const { Telegraf } = require('telegraf');
-const axios = require('axios');
 
 const API_KEY = '74dc824830c7f93dc61b03e324070886';
 const BOT_TOKEN = '7799058013:AAE-HjGVOt5AbOKfdR-KGL66reWhdADPgn8';
 
-const bot = new Telegraf(BOT_TOKEN);
+const { Telegraf } = require('telegraf');
 
-bot.on('inline_query', async (ctx) => {
- const query = ctx.inlineQuery.query;
- if (!query) return;
+const bot = new Telegraf('BOT_TOKEN');
 
- const response = await axios.get(`https://api.themoviedb.org/3/search/movie?api_key=${API_KEY}&language=es-ES&query=${query}`);
- const movies = response.data.results;
-
- const results = movies.map(movie => ({
-  type: 'article',
-  id: movie.id,
-  title: movie.title,
-  input_message_content: {
-   message_text: `
-        ![Backdrop](${movie.backdrop_path ? `https://image.tmdb.org/t/p/w500${movie.backdrop_path}` : ''})
-        **${movie.title} (${movie.release_date.split('-')[0]})**
-        - Título original: ${movie.original_title}
-        - Idioma original: ${movie.original_language}
-        - Duración: ${movie.runtime} min
-        - Géneros: ${movie.genre_ids.join(', ')}
-        - Sinopsis: ${movie.overview}
-      `,
-  },
-  thumb_url: movie.backdrop_path ? `https://image.tmdb.org/t/p/w500${movie.backdrop_path}` : '',
-  description: movie.title,
- }));
-
- await ctx.answerInlineQuery(results);
+bot.on('text', (ctx) => {
+    ctx.reply(ctx.message.text);
 });
 
 bot.launch();
