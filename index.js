9ee2379a94982b261a9cac171669d00a662ddc53
fsfeds95 <144530359+fsfeds95@users.noreply.github.com>
2024-09-23 20:10:57 -0400

    Actualizar index.js

--- index.js
+++ index.js
@@ -1,26 +1,18 @@
+const BOT_TOKEN = '7299943772:AAGtB60BK_9GY43LcotfNNSRy0qxB1CzPZQ';
+
 const { Telegraf } = require('telegraf');
-const axios = require('axios');
 
-const bot = new Telegraf('7299943772:AAFMZ8yI9RVwpG9GNXQBpZsMJTRW0S05kCM');
-const apiKey = '74dc824830c7f93dc61b03e324070886';
+const bot = new Telegraf(BOT_TOKEN);
 
-bot.command('pelicula', async (ctx) => {
- const query = ctx.message.text.split(' ').slice(1).join(' ');
+bot.start((ctx) => {
+ const username = ctx.from.username ? `@${ctx.from.username}` : '';
+ const firstName = ctx.from.first_name ? ctx.from.first_name : '';
 
- try {
-  const response = await axios.get(`https://api.themoviedb.org/3/search/movie?api_key=${apiKey}&query=${query}`);
+ ctx.reply(`¡Hola ${firstName}, este es tu usuario ${username}!`);
+});
 
-  const movies = response.data.results;
-
-  const results = movies.map(movie => {
-   return `${movie.title} (${movie.release_date.split('-')[0]})\n${movie.original_title}\nIdioma: ${movie.original_language}\nGéneros: ${movie.genre_ids.join(', ')}\n${movie.overview}\n\n`;
-  });
-
-  ctx.reply('Resultados de la búsqueda:\n\n' + results.join('\n\n'));
- } catch (error) {
-  console.log(error);
-  ctx.reply('Lo siento, hubo un error al buscar la película.');
- }
+bot.on('text', (ctx) => {
+ ctx.reply('' + ctx.message.text);
 });
 
 bot.launch();
