b0cf3d6f86087f952f659f6422db1dd75a198add
fsfeds95 <144530359+fsfeds95@users.noreply.github.com>
2024-09-24 00:09:13 -0400

    Actualizar index.js

--- index.js
+++ index.js
@@ -1,12 +1,11 @@
 // Importar las bibliotecas requeridas
 const express = require('express');
-const { Jimp } = require('jimp-compact');
-
 // Crea una aplicación en Express
 const app = express();
 const port = 8225;
 
 
+// Importar la biblioteca de Telegraf
 const { Telegraf } = require('telegraf');
 
 const BOT_TOKEN = '7299943772:AAGtB60BK_9GY43LcotfNNSRy0qxB1CzPZQ';
@@ -18,60 +17,6 @@ bot.start((ctx) => {
  const firstName = ctx.from.first_name ? ctx.from.first_name : '';
 
  ctx.reply(`¡Hola ${firstName}, este es tu usuario ${username}!`);
-});
-
-bot.command('backdrop', async (ctx) => {
- const repliedMessage = ctx.message.reply_to_message;
-
- if (repliedMessage && repliedMessage.photo) {
-  const photo = repliedMessage.photo[repliedMessage.photo.length - 1]; // Obtiene la última foto respondida
-
-  try {
-   const imageUrl = await ctx.telegram.getFileLink(photo.file_id);
-   const image = await Jimp.read(imageUrl);
-
-   // Redimensionar la imagen usando RESIZE_MAGPHASE
-   image.resize(1280, 720, Jimp.RESIZE_MAGPHASE);
-
-   // Cargar las marcas de agua
-   const watermark1 = await Jimp.read('./img/b/Wtxt-Backdrop.png');
-   const watermark2 = await Jimp.read('./img/b/Wlogo-Backdrop.png');
-
-   // Escala la marca de agua a 1280px de ancho por 720px de alto
-   watermark1.resize(1280, 720);
-   watermark2.resize(1280, 720);
-
-   // Establece la opacidad de la watermark1 a 0.375 y watermark2 a 0.75
-   watermark1.opacity(0.08);
-   watermark2.opacity(0.40);
-
-   // Combinar las marcas de agua en una sola imagen
-   watermark1.composite(watermark2, 0, 0, {
-    mode: Jimp.BLEND_SOURCE_OVER,
-    opacitySource: 1.0,
-    opacityDest: 1.0
-   });
-
-   // Aplicar la marca de agua a la imagen
-   image.composite(watermark1, 0, 0, {
-    mode: Jimp.BLEND_SOURCE_OVER,
-    opacitySource: 1.0,
-    opacityDest: 1.0
-   });
-
-   // Guardar la imagen en formato WEBP con calidad al 100%
-   image.quality(100).scale(1);
-
-   const buffer = await image.getBufferAsync(Jimp.MIME_JPEG);
-
-   ctx.replyWithPhoto({ source: buffer });
-  } catch (error) {
-   console.log(error);
-   ctx.reply('Hubo un error al agregar la marca de agua a la imagen.');
-  }
- } else {
-  ctx.reply('Por favor, responde a una imagen para agregarle una marca de agua utilizando el comando /backdrop.');
- }
 });
 
 bot.on('sticker', (ctx) => {
