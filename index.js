5f57c6d3226966a97277f9db7ba5bbdcf6342db2
fsfeds95 <144530359+fsfeds95@users.noreply.github.com>
2024-10-08 00:58:00 -0400

    Actualizar index.js

--- index.js
+++ index.js
@@ -19,26 +19,24 @@ const ALLOWED_USER_ID = 6839704393; // Reemplaza con el ID del usuario permitido
 
 // URLs de los feeds RSS
 // Comando para obtener todos los artículos
-const RSS_all = 'https://www.cinemascomics.com/feed/';
+const RSS_all_1 = 'https://www.cinemascomics.com/feed/';
+// Comando para obtener todos los artículos
+const RSS_all_2 = 'https://spoiler.bolavip.com/rss/feed';
+
 // Comando para obtener artículos de cine
 const RSS_cine = 'https://www.cinemascomics.com/cine/feed/';
 // Comando para obtener artículos de series
 const RSS_serie = 'https://www.cinemascomics.com/series-de-television/feed/';
 
 // Conjuntos para almacenar los IDs de los artículos enviados
-const sentCineIds = new Set();
+const sentCinemascomicsIds = new Set();
+const sentSpoilerIds = new Set();
 const sentSerieIds = new Set();
 
 // Configuración del limitador
 const limiter = new Bottleneck({
  minTime: 30000, // Espera 30 segundo entre cada solicitud
 });
-
-// Función para extraer la URL de la primera imagen del contenido
-const extractImage = (content) => {
- const match = content.match(/<img[^>]+src="([^">]+)"/);
- return match ? match[1] : null; // Retorna la URL de la primera imagen o null
-};
 
 // Función para verificar si la URL de la imagen es válida
 const isValidImageUrl = (url, callback) => {
@@ -48,26 +46,101 @@ const isValidImageUrl = (url, callback) => {
 };
 
 // Función para obtener y enviar artículos de cine
-const fetchNews = (ctx = null) => {
+const fetchCinemascomics = (ctx = null) => {
  return new Promise((resolve, reject) => {
-  request(RSS_all, (error, response, body) => {
+  request(RSS_all_1, (error, response, body) => {
    if (!error && response.statusCode === 200) {
     xml2js.parseString(body, (err, result) => {
      if (!err) {
       const items = result.rss.channel[0].item; // Obtiene los artículos del feed
-      const randomArticles = items.sort(() => 0.5 - Math.random()).slice(0, 3); // Artículos aleatorios
+      const randomArticles = items.sort(() => 0.6 - Math.random()).slice(0, 6); // Artículos aleatorios
 
       randomArticles.forEach(item => {
        const id = item.link[0]; // Usamos el enlace como ID único
-       if (!sentCineIds.has(id)) { // Verificamos si ya fue enviado
-        sentCineIds.add(id); // Añadimos a los enviados
+       if (!sentCinemascomicsIds.has(id)) { // Verificamos si ya fue enviado
+        sentCinemascomicsIds.add(id); // Añadimos a los enviados
 
         // Extraemos información del artículo
         const title = item.title[0];
         const link = item.link[0];
         const description = item.description[0];
         const content = item['content:encoded'][0];
-        const imageUrl = extractImage(content); // Obtener la imagen
+        const imageUrl = extractImage_1(content); // Obtener la imagen
+        const hashtags = ['#Cine', '#Noticias', '#Películas', '#Estrenos', '#Cultura', '#Entretenimiento', '#introCinemaClub'];
+
+        // Procesar categorías para crear hashtags
+        const categoriesText = item.category ? item.category : [];
+        const catReplace = categoriesText.join(' ').replace(/\s/g, '_');
+        const hashtagCat = `#` + catReplace.split('_').join(' #');
+
+        const uniqueHashtags = new Set(hashtags);
+        hashtagCat.split(' ').forEach(cat => {
+         if (cat) {
+          uniqueHashtags.delete(cat); // Elimina si ya existe
+         }
+        });
+
+        const finalHashtags = Array.from(uniqueHashtags).join(' '); // Combina los hashtags únicos
+
+        const message = `
+⟨📰⟩ #Noticia
+▬▬▬▬▬▬▬▬▬
+⟨🍿⟩ ${title}
+▬▬▬▬▬▬▬▬▬
+⟨💭⟩ Resumen: ${description.substring(0, 1500)}...
+▬▬▬▬▬▬▬▬▬
+${finalHashtags}
+▬▬▬▬▬▬▬▬▬
+`;
+
+        // Verificar si la URL de la imagen es válida
+        isValidImageUrl(imageUrl, (isValid) => {
+         if (isValid) {
+          // Crear un botón para el enlace
+          const button = [{ text: '⟨🗞️⟩ Leer más ⟨🗞️⟩', url: link }];
+          bot.telegram.sendPhoto(ctx.chat.id, imageUrl, { caption: message, reply_markup: { inline_keyboard: [button] } })
+           .catch(err => console.error('Error al enviar el mensaje:', err)); // Manejo de errores
+         } else {
+          console.error('URL de imagen no válida:', imageUrl);
+         }
+        });
+       }
+      });
+      resolve(); // Resuelve la promesa
+     } else {
+      console.error('Error al parsear el RSS:', err);
+      reject(err); // Rechaza la promesa
+     }
+    });
+   } else {
+    console.error('Error al obtener el RSS:', error);
+    reject(error); // Rechaza la promesa
+   }
+  });
+ });
+};
+
+// Función para obtener y enviar artículos
+const fetchSpoiler = (ctx = null) => {
+ return new Promise((resolve, reject) => {
+  request(RSS_all_2, (error, response, body) => {
+   if (!error && response.statusCode === 200) {
+    xml2js.parseString(body, (err, result) => {
+     if (!err) {
+      const items = result.rss.channel[0].item; // Obtiene los artículos del feed
+      const randomArticles = items.sort(() => 0.6 - Math.random()).slice(0, 6); // Artículos aleatorios
+
+      randomArticles.forEach(item => {
+       const id = item.link[0]; // Usamos el enlace como ID único
+       if (!sentIds.has(id)) { // Verificamos si ya fue enviado
+        sentIds.add(id); // Añadimos a los enviados
+
+        // Extraemos información del artículo
+        const title = item.title[0];
+        const link = item.link[0];
+        const description = item.description[0].replace(/<[^>]*>/g, '');
+        const imageUrl = item['media:content'] ? item['media:content'][0].$.url : null;
+
         const hashtags = ['#Cine', '#Noticias', '#Películas', '#Estrenos', '#Cultura', '#Entretenimiento', '#introCinemaClub'];
 
         // Procesar categorías para crear hashtags
@@ -134,15 +207,15 @@ const fetchCine = (ctx = null) => {
 
       randomArticles.forEach(item => {
        const id = item.link[0]; // Usamos el enlace como ID único
-       if (!sentCineIds.has(id)) { // Verificamos si ya fue enviado
-        sentCineIds.add(id); // Añadimos a los enviados
+       if (!sentCinemascomicsIds.has(id)) { // Verificamos si ya fue enviado
+        sentCinemascomicsIds.add(id); // Añadimos a los enviados
 
         // Extraemos información del artículo
         const title = item.title[0];
         const link = item.link[0];
         const description = item.description[0];
         const content = item['content:encoded'][0];
-        const imageUrl = extractImage(content); // Obtener la imagen
+        const imageUrl = extractImage_1(content); // Obtener la imagen
         const hashtags = ['#Cine', '#Noticias', '#Películas', '#Estrenos', '#Cultura', '#Entretenimiento', '#introCinemaClub'];
 
         // Procesar categorías para crear hashtags
@@ -217,8 +290,8 @@ const fetchSerie = (ctx = null) => {
         const link = item.link[0];
         const description = item.description[0];
         const content = item['content:encoded'][0];
-        const imageUrl = extractImage(content); // Obtener la imagen
-        const hashtags = ['#Cine', '#Noticias', '#Películas', '#Estrenos', '#Cultura', '#Entretenimiento', '#introCinemaClub'];
+        const imageUrl = extractImage_1(content); // Obtener la imagen
+        const hashtags = ['#Cine', '#Noticias', '#Películas', '#Estrenos', '#Cultura', '#Entretenimiento', '#introCinemaClub', '#Serie'];
 
         // Procesar categorías para crear hashtags
         const categoriesText = item.category ? item.category : [];
@@ -283,7 +356,7 @@ bot.start((ctx) => {
  ctx.reply('¡Hola! Estoy aquí para traerte artículos de cine y series.\n\nPuedes usar el comando /cine para obtener 3 artículos de cine aleatorias.\n\nPuedes usar el comando /serie para obtener 3 artículos de series aleatorias')
 });
 
-bot.command('news', (ctx) => {
+bot.command('cinemascomics', (ctx) => {
  if (ctx.from.id !== ALLOWED_USER_ID) {
   return ctx.reply('Lo siento, no tienes permiso para usar este comando.'); // Mensaje de error
  }
@@ -293,10 +366,26 @@ bot.command('news', (ctx) => {
  const firstName = ctx.from.first_name ? ctx.from.first_name : '';
  const userId = ctx.from.id;
 
- console.log(`"Nombre: ${firstName}, Usuario: ${username}, con el id: ${userId} uso : /news"`);
+ console.log(`"Nombre: ${firstName}, Usuario: ${username}, con el id: ${userId} uso : /cinemascomics"`);
 
- limiter.schedule(() => fetchNews(ctx)).catch(err => console.error('Error en fetchCine:', err));
+ limiter.schedule(() => fetchCinemascomics(ctx)).catch(err => console.error('Error en fetchCinemascomics:', err));
 });
+
+bot.command('spoiler', (ctx) => {
+ if (ctx.from.id !== ALLOWED_USER_ID) {
+  return ctx.reply('Lo siento, no tienes permiso para usar este comando.'); // Mensaje de error
+ }
+
+ lastCtx = ctx; // Guarda el contexto
+ const username = ctx.from.username ? `@${ctx.from.username}` : '';
+ const firstName = ctx.from.first_name ? ctx.from.first_name : '';
+ const userId = ctx.from.id;
+
+ console.log(`"Nombre: ${firstName}, Usuario: ${username}, con el id: ${userId} uso : /spoiler"`);
+ 
+ limiter.schedule(() => fetchSpoiler(ctx)).catch(err => console.error('Error en fetchSpoiler:', err));
+})
+
 bot.command('cine', (ctx) => {
  if (ctx.from.id !== ALLOWED_USER_ID) {
   return ctx.reply('Lo siento, no tienes permiso para usar este comando.'); // Mensaje de error
@@ -311,6 +400,7 @@ bot.command('cine', (ctx) => {
 
  limiter.schedule(() => fetchCine(ctx)).catch(err => console.error('Error en fetchCine:', err));
 });
+
 bot.command('serie', (ctx) => {
  if (ctx.from.id !== ALLOWED_USER_ID) {
   return ctx.reply('Lo siento, no tienes permiso para usar este comando.'); // Mensaje de error
@@ -366,16 +456,16 @@ bot.on('message', (ctx) => {
 // Mantiene el bot vivo y envía artículos cada 12 horas
 setInterval(() => {
  if (lastCtx) {
-  limiter.schedule(() => fetchCine(lastCtx)).catch(err => console.error('Error en fetchCine desde intervalo:', err));
+  limiter.schedule(() => fetchCinemascomics(lastCtx)).catch(err => console.error('Error en fetchCine desde intervalo:', err));
  }
 }, 43200000); // Cada 12 horas
 
-// Mantiene el bot vivo y envía artículos cada 24 horas
+// Mantiene el bot vivo y envía artículos cada 12 horas
 setInterval(() => {
  if (lastCtx) {
-  limiter.schedule(() => fetchSerie(lastCtx)).catch(err => console.error('Error en fetchSerie desde intervalo:', err));
+  limiter.schedule(() => fetchSpoiler(lastCtx)).catch(err => console.error('Error en fetchSerie desde intervalo:', err));
  }
-}, 86400000); // Cada 24 horas
+}, 43200000); // Cada 12 horas
 
 bot.launch(); // Inicia el bot
 
@@ -402,3 +492,9 @@ app.listen(port, () => {
    });
  }, 5 * 60 * 1000); // Cada 5 minutos
 });
+
+// Función para extraer la URL de la primera imagen del contenido
+const extractImage_1 = (content) => {
+ const match = content.match(/<img[^>]+src="([^">]+)"/);
+ return match ? match[1] : null; // Retorna la URL de la primera imagen o null
+};
